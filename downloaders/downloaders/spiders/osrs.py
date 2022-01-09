7144fa16ff52b842715dd9af8069a9e890aaf13d
Nicholas Bryant <nicholasbryantnab@gmail.com>
2022-01-08 23:23:26 -0500

    bazinga

--- downloaders/downloaders/spiders/osrs.py
+++ downloaders/downloaders/spiders/osrs.py
@@ -1,4 +1,5 @@
-from scrapy import Request, Spider
+from scrapy import Request, Spider, signals
+from scrapy.exceptions import DontCloseSpider
 
 
 class OsrsSpider(Spider):
@@ -6,11 +7,30 @@ class OsrsSpider(Spider):
     allowed_domains = ['runescape.com']
     start_urls = ['https://oldschool.runescape.com/']
 
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        spider = super(OsrsSpider, cls).from_crawler(crawler, *args, **kwargs)
+        crawler.signals.connect(spider.spider_idle, signal=signals.spider_idle)
+        return spider
+
+    def __init__(self):
+        self.start_url = 'https://secure.runescape.com/m=news/archive?oldschool=1'
+        self.seen = []
+
     def parse(self, response):
-        print(response.body)
+        headlines = response.xpath('//*[@id="newsSection"]/article/div/header/h4/a')
+        for headline in headlines:
+            url = headline.xpath('./@href').extract_first()
+            if url not in self.seen:
+                self.seen.append(url)
+                print("New Update Found @ " + url)
+
+    def create_start_request(self):
+        return Request(self.start_url, self.parse, dont_filter=True)
 
     def start_requests(self):
-        yield Request(
-            'https://oldschool.runescape.com/',
-            self.parse
-        )
+        yield self.create_start_request()
+
+    def spider_idle(self):
+        self.crawler.engine.crawl(self.create_start_request(), spider=self)
+        raise DontCloseSpider("Checking for new updates")
